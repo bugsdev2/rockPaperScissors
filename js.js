6fd64dfa37bae13b551ff012de176582b5983203
BugsDev2 <msherlock246@gmail.com>
2022-11-03 22:09:31 +0530

    Made modifications to the style and added a h3 element containing the written status of the match.

--- js.js
+++ js.js
@@ -13,16 +13,19 @@ const userPlayArea = document.querySelector('.play-area.user')
 const userTableData = document.querySelector('#user');
 const compTableData = document.querySelector('#comp');
 
+const h3 = document.querySelector('.table h3');
 
 options.forEach(option => option.addEventListener('click', startGameplay))
 
 function startGameplay(e) {
 	if (youCount == 5 || compCount == 5) {
-		alert('Do you want to start a new game?');
-		youCount=0; 
-		compCount = 0;
-		userTableData.textContent = youCount;
-		compTableData.textContent = compCount;
+		if (confirm('Do you want to start a new game?')) {
+			youCount=0; 
+			compCount = 0;
+			userTableData.textContent = youCount;
+			compTableData.textContent = compCount;
+			h3.textContent = '';
+		};
 	} else {
 		const userChoice = e.target.getAttribute('data-id');
 		let computerChoice = items[random(3)];
@@ -41,36 +44,42 @@ function startGameplay(e) {
 		compPlayArea.replaceChild(imgComp, compPlayArea.firstElementChild);
 		
 			if (userChoice === computerChoice) {
-				return;
+				h3.textContent = "It's a tie!";
 			} else {
 				if (userChoice == "rock" && computerChoice == "paper") {
+					h3.textContent = "You lose!";
 					loser = userChoice;
 					imgCross.setAttribute('src', `assets/images/${loser}Cross.png`);
 					userPlayArea.replaceChild(imgCross, userPlayArea.firstElementChild);
 					compCount+=1;
 				} else if (userChoice == "paper" && computerChoice == "rock") {
+					h3.textContent = "You win!";
 					loser = computerChoice;
 					imgCross.setAttribute('src', `assets/images/${loser}Cross.png`);
 					compPlayArea.replaceChild(imgCross, compPlayArea.firstElementChild);
 					youCount+=1;
 				}
 				if (userChoice == "paper" && computerChoice == "scissors") {
+					h3.textContent = "You lose!";
 					loser = userChoice;
 					imgCross.setAttribute('src', `assets/images/${loser}Cross.png`);
 					userPlayArea.replaceChild(imgCross, userPlayArea.firstElementChild);
 					compCount+=1;
 				} else if (userChoice == "scissors" && computerChoice == "paper") {
+					h3.textContent = "You win!";
 					loser = computerChoice;
 					imgCross.setAttribute('src', `assets/images/${loser}Cross.png`);
 					compPlayArea.replaceChild(imgCross, compPlayArea.firstElementChild);
 					youCount+=1;
 				}
 				if (userChoice == "scissors" && computerChoice == "rock") {
+					h3.textContent = "You lose!";
 					loser = userChoice;
 					imgCross.setAttribute('src', `assets/images/${loser}Cross.png`);
 					userPlayArea.replaceChild(imgCross, userPlayArea.firstElementChild);
 					compCount+=1;
 				} else if (userChoice == "rock" && computerChoice == "scissors") {
+					h3.textContent = "You win!";
 					loser = computerChoice;
 					imgCross.setAttribute('src', `assets/images/${loser}Cross.png`);
 					compPlayArea.replaceChild(imgCross, compPlayArea.firstElementChild);
